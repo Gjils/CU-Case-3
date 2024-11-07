0bb1dd1c04eb1741e7f2951313f76e7c9d2f2bac
Realtod <89342053+Realtod@users.noreply.github.com>
2024-11-08 01:54:21 +0300

    Update ContrlolSum.py

--- encrypt-package/ContrlolSum.py
+++ encrypt-package/ContrlolSum.py
@@ -1,7 +1,19 @@
 import hashlib
 
-init_data = input()
-encrypt_data = input()
+def calculate_sha256_checksum(file_path):
+  sha256_hash = hashlib.sha256()
+  with open(file_path, 'rb') as f:
+    for byte_block in iter(lambda: f.read(4096), b""):
+      sha256_hash.update(byte_block)
+  return sha256_hash.hexdigit()
 
-if hashlib.sha256(init_data) == hashlib.sha256(encrypt_data):
-  print("yes")
+original_file = ''
+original_hashsum = calculate_sha256_checksum(original_file)
+
+encrypt_file = ''
+encrypt_hashsum = calculate_sha256_checksum(encrypt_file)
+
+if original_file == encrypt_file:
+  print("das ist good")
+else:
+  print("not good")
